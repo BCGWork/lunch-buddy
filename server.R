a374bcab48dcfa897d42b770755dd659e18804b0
Boxuan Cui <boxuancui@gmail.com>
2015-01-08 17:05:52 -0500

    move yelp data inside server

--- server.R
+++ server.R
@@ -6,11 +6,6 @@ buddy <- read.csv("data/member.csv")
 buddy$id <- 1:nrow(buddy)
 kNumBuddy <- nrow(buddy) - 1
 
-place.json <- fromJSON(file = "data/restaurant_data.txt")
-place <- lapply(place.json, function(x) {data.frame("name"=x$name, "address"=x$address)})
-restaurant <- do.call(rbind, place)
-kNumPlace <- nrow(restaurant)
-
 office <- "53 State Street Boston MA"
 office.gc <- geocode(office)
 
@@ -18,12 +13,17 @@ shinyServer(function(input, output) {
   data.object <- reactive({
     input$find
     isolate({
+      place.json <- fromJSON(file = "data/restaurant_data.txt")
+      place <- lapply(place.json, function(x) {data.frame("name"=x$name, "address"=x$address)})
+      restaurant <- do.call(rbind, place)
+      kNumPlace <- nrow(restaurant)
+      
       buddy.pool <- subset(buddy$id, buddy$nickname!=input$user)
       kBuddyInd <- sample(buddy.pool, input$num)
       kRestInd <- sample(1:kNumPlace, 1)
       buddy.list <- as.character(buddy[kBuddyInd, ]$nickname)[order(as.character(buddy[kBuddyInd, ]$nickname))]
       email.list <- paste(paste0(buddy[kBuddyInd, ]$lastname, ".", buddy[kBuddyInd, ]$firstname, "@bcg.com"), collapse = ", ")
-      return(list("kRestInd" = kRestInd, "buddy.list" = buddy.list, "email.list" = email.list))
+      return(list("kRestInd" = kRestInd, "buddy.list" = buddy.list, "email.list" = email.list, "restaurant" = restaurant))
     })
   })
   
@@ -31,6 +31,7 @@ shinyServer(function(input, output) {
     data <- data.object()
     kRestInd <- data$kRestInd
     buddy.list <- data$buddy.list
+    restaurant <- data$restaurant
     
     cat(
       paste0(
@@ -48,6 +49,7 @@ shinyServer(function(input, output) {
   output$static_map <- renderPlot({
     data <- data.object()
     kRestInd <- data$kRestInd
+    restaurant <- data$restaurant
     
     restaurant.gc <- geocode(as.character(restaurant$address[kRestInd]))
     gc.df <- data.frame(rbind(office.gc, restaurant.gc))
@@ -60,6 +62,7 @@ shinyServer(function(input, output) {
     data <- data.object()
     kRestInd <- data$kRestInd
     email.list <- data$email.list
+    restaurant <- data$restaurant
     
     cat(paste0("<a href='mailto:", email.list, "?Subject=You are my lunch buddy today!&body=Hey! Wanna grab lunch at ", gsub("'", "", as.character(restaurant$name[kRestInd])), " with me?'>Email buddies?</a>"))
     cat("<br/>")
--- ui.R
+++ ui.R
@@ -2,28 +2,34 @@ library(shiny)
 buddy <- read.csv("data/member.csv")
 numBuddy <- nrow(buddy) - 1
 
-shinyUI(fluidPage(
-  titlePanel("Lunch Buddy"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("user", label="Who are you?", as.character(buddy$nickname)[order(as.character(buddy$nickname))]),
-      sliderInput("num", label="Number of buddies:", min=1, max=numBuddy, value=1, step=1),
-      br(),
-      actionButton("find", "Try again")
-    ),
-    
-    mainPanel(
-      h4(paste0("Greetings! Look for lunch suggestions on ", Sys.Date(), "?")),
-      br(),
-      br(),
-      htmlOutput("result"),
-      br(),
-      plotOutput("static_map", height="640px"),
-      br(),
-      htmlOutput("email")
+shinyUI(
+  fluidPage(
+    theme="bootstrap.min.css",
+    navbarPage(
+      "Lunch Buddy",
+      tabPanel(
+        "App",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput("user", label="Who are you?", as.character(buddy$nickname)[order(as.character(buddy$nickname))]),
+            sliderInput("num", label="Number of buddies:", min=1, max=numBuddy, value=1, step=1),
+            br(),
+            actionButton("find", "Try again")
+          ),
+          
+          mainPanel(
+            tags$body(tags$script(src="www/lb.js")),
+            h4(paste0("Greetings! Look for lunch suggestions on ", Sys.Date(), "?")),
+            br(),
+            br(),
+            htmlOutput("result"),
+            plotOutput("static_map", height="640px"),
+            htmlOutput("email")
+          )
+        )
+      ),
+      tabPanel("About")
     )
   )
-))
-
+)
 
